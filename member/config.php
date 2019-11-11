fe40e485e242723bc63e7af1de68b5cd572cd10a
Bashir Hassan <bashhassan2020@gmail.com>
2019-11-11 13:52:10 +0100

    update

--- member/config.php
+++ member/config.php
@@ -1,7 +1,7 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-<<<<<<< HEAD
+
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
@@ -9,17 +9,6 @@ define('DB_NAME', 'alumni');
  
 /* Attempt to connect to MySQL database */
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-// $mysqli = new mysqli('localhost','root','','alumni');
-=======
-// define('DB_SERVER', 'localhost');
-// define('DB_USERNAME', 'root');
-// define('DB_PASSWORD', '');
-// define('DB_NAME', 'alumni');
- 
-/* Attempt to connect to MySQL database */
-// $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-$mysqli = new mysqli('localhost','root','','alumni');
->>>>>>> master
  
 // Check connection
 if($mysqli === false){
